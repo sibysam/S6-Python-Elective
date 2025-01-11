30d142bc9b737d22f55c79722c8c087fd1fa1cd5
sibysam <160556901+sibysam@users.noreply.github.com>
2025-01-11 23:19:16 +0530

    income_tax

--- IncomeTaxCalculator.py
+++ IncomeTaxCalculator.py
@@ -3,20 +3,18 @@ def calculate(amt,per):
 
 def calc_income_tax(total):
 
-    if total <=250000:
+    if total <=300000:
         return 0
-    elif total <= 500000:
-        return calculate(total-250000,5)
-    elif total <= 750000:
-        return calculate(total-500000,10)+12500
+    elif total <= 700000:
+        return calculate(total-300000,5)
     elif total <= 1000000:
-        return calculate(total-750000,15)+37500
-    elif total <= 1250000:
-        return calculate(total-1000000,20)+75000
+        return calculate(total-700000,10)+20000
+    elif total <= 1200000:
+        return calculate(total-1000000,15)+50000
     elif total <= 1500000:
-        return calculate(total-1250000,25)+125000
+        return calculate(total-1200000,20)+80000
     else:
-        return calculate(total-1500000,30)+187500
+        return calculate(total-1500000,30)+140000
 
 
 total=float(input("Enter Annual Income : "))
